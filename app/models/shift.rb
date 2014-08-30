8e0fb9f6f2664cb804e4db915a13cf1bc2664d17
sbr <sharpebr@icloud.com>
2014-08-30 12:37:05 -0700

    added json configuration to shift model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,6 +5,14 @@ class Shift < ActiveRecord::Base
   belongs_to :employee
 
   validates_presence_of :employee_id, :store_id
+  def as_json(options = {})
+    { :id => self.id, 
+      :start_date => self.start_date,
+      :end_date => self.end_date,
+      :text => "it worked",
+    }
+    # strftime('%Y-%m-%d %H:%M:%S')
+  end
 
 end
 
@@ -12,10 +20,15 @@ ActiveSupport.use_standard_json_time_format = false
 
 class ActiveSupport::TimeWithZone
   def as_json(options = {})
+    { :id => self.id, 
+      :start_date => self.start_date,
+      :end_date => self.end_date,
+      :text => "it worked",
+    }
     # strftime('%Y-%m-%d %H:%M:%S')
   end
 end
 
-class ShiftSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :updated_at, :store_id, :employee_id, :created_at
-end
+# class ShiftSerializer < ActiveModel::Serializer
+#   attributes :id, :start_date, :end_date, :updated_at, :store_id, :employee_id, :created_at
+# end
